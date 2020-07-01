8bc5904e8c28de5a5f0a4e8675415a59923f7c96
Chao <c.xu-6@student.tudelft.nl>
2020-07-01 21:54:16 +0800

    key

--- RSA/main3.cpp
+++ RSA/main3.cpp
@@ -1,74 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
-#include <gmp.h>
+//#include <gmp.h>
 #include <cmath>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
 void GenerateKey(){
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
-    mpz_t p;
-    mpz_t q;
-    mpz_t n;
-    mpz_init(p);
-    mpz_init(q);
-    mpz_init(n);
-    while (mpz_sizeinbase(n, 2) < 1024) { //keep generating until n meets the security level
-        mpz_urandomb(p, state, 1024);
-        while (mpz_probab_prime_p(p, 40) == 0) {
-            mpz_urandomb(p, state, 1024);
-        } //keep generating p until p is a prime number
-        mpz_urandomb(q, state, 1024);
-        while (mpz_probab_prime_p(q, 40) == 0) {
-            mpz_urandomb(q, state, 1024);
-        } //keep generating q until q is a prime number
-        mpz_mul(n, p, q); //calculate n
-    }
-    //calculate remaining variables
-    mpz_t phi;
-    mpz_t e;
-    mpz_t d;
-    mpz_init(phi);
-    mpz_init(e);
-    mpz_init(d);
-    mpz_t temp1;
-    mpz_t temp2;
-    mpz_init(temp1);
-    mpz_init(temp2);
-    //phi
-    mpz_sub_ui(temp1, p, 1);
-    mpz_sub_ui(temp2, q, 1);
-    mpz_mul(phi, temp1, temp2);
-    //e, assume e is a 16-bit number
-    mpz_urandomb(e, state, 16);
-    mpz_gcd(temp1, e, phi);
-    while (mpz_cmp_ui(temp1, 1) != 0) {
-        mpz_urandomb(e, state, 16);
-        mpz_gcd(temp1, e, phi);
-    }
-    //d
-    mpz_invert(d, e, phi);
-    gmp_printf("%Zd \n %Zd \n %Zd \n %Zd \n %Zd \n %Zd \n", p, q, n, phi, e, d);
-    mpz_clear(p);
-    mpz_clear(q);
-    mpz_clear(n);
-    mpz_clear(phi);
-    mpz_clear(e);
-    mpz_clear(d);
-    mpz_clear(temp1);
-    mpz_clear(temp2);
+    
+    int i=1;
+    srand((int)time(0));
+    int a;
+    int b;
+    for(i=0;i<1671;i++)
+    {
+	
+    b=rand() % 70;
+    if(b<26) a=(rand() % (90-65+1))+ 65; 
+    else if (b<38) a=(rand() % (57-48+1))+ 48; 
+    else if (b<64)a=(rand() % (122-97+1))+ 97; 
+    else 
+    {
+    	int mark=rand()%7;
+    	if(mark==0) a=',';
+		else if(mark==1) a='.';
+		else if(mark==2) a=';';
+		else if(mark==3) a='?';
+		else if(mark==4) a='!';
+		else if(mark==5) a='(';	
+		else a=')';
+		}
+    cout<<(char)a;
+	}
+    
 }
-
+/*
 void encrypt(const char * message, string key){
     //read key
     ifstream keyfile;
     keyfile.open(key);
     string p0, q0, n0, phi0, e0, d0;
     keyfile >> p0 >> q0 >> n0 >> phi0 >> e0 >> d0;
-    const char* e1 = e0.c_str();
-    const char* n1 = n0.c_str();
+    int  
     //encryption
     mpz_t m;
     mpz_t c;
@@ -117,9 +92,9 @@ void decrypt(const char * message, string key){
     mpz_clear(d);
     mpz_clear(n);
 }
-
+*/
 int main(int argu, char * argv[]) {
-    if (argu == 2) GenerateKey();
+    /*if (argu == 2) GenerateKey();
     else if (argu == 3 && argv[1][2] == 'e') encrypt(argv[2], "keyfile.txt");
     else if (argu == 3 && argv[1][2] == 'd') {
         //test if is using the default key to decript the ciphertext
@@ -145,5 +120,10 @@ int main(int argu, char * argv[]) {
     }
     else if (argu == 5 && argv[1][2] == 'e') encrypt(argv[2], argv[4]);
     else if (argu == 5 && argv[1][2] == 'd') decrypt(argv[2], argv[4]);
-    return 0;
+    return 0;*/
+    GenerateKey();
 }
+
+
+
+
